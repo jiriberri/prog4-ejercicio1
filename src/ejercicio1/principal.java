49d4dfc7b1fa5153de3fe2e9a318cd68ac066a18
Franco Brandan <brandanfranco0@gmail.com>
2026-08-21 21:44:14 +0200

    Subo Ejercicio 1 y ordeno la estructura del proyecto

--- src/ejercicio1/principal.java
+++ src/ejercicio1/principal.java
@@ -5,6 +5,10 @@ public class principal {
 	public static void main(String[] args) {
 		
 		System.out.println("--- Proyecto Inicializado por Grupo 18 ---");
+		
+		Persona p1 = new Persona();
+		p1.setNombre("Juan");
+		System.out.println(p1.toString());
 
 	}
 
